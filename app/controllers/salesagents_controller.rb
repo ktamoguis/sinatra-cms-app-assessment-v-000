3bdd10448d9033fe2983efba95a09f122813eed1
Karl Amoguis <ktamoguis@gmail.com>
2018-06-30 14:57:36 +0000

    Done.

--- app/controllers/salesagents_controller.rb
+++ app/controllers/salesagents_controller.rb
@@ -9,15 +9,15 @@ class AgentsController < ApplicationController
   end
 
   post '/agents/new' do
-    if params[:username] == "" || params[:email] == "" || params[:password] == ""
-    redirect to("/signup")
-  else
-    @user = User.create(params)
-    @user.save
-    session[:user_id] = @user.id
-  end
+    if params[:username] == "" || params[:password] == ""
+      redirect to("/signup")
+    else
+      @user = User.create(params)
+      @user.save
+      session[:user_id] = @user.id
+    end
 
-  redirect to("/leads")
+    redirect to("/leads")
 
   end
 

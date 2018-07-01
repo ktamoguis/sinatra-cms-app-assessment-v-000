945851a135002c5ba588f9f8596f170d9ffbab02
Karl Amoguis <ktamoguis@gmail.com>
2018-07-01 04:26:07 +0000

    Done.

--- app/controllers/salesagents_controller.rb
+++ app/controllers/salesagents_controller.rb
@@ -18,7 +18,34 @@ class AgentsController < ApplicationController
     end
 
     redirect to("/leads")
+  end
 
+  get '/login' do
+    #binding.pry
+    if logged_in?
+      redirect to("/leads")
+    end
+
+    erb :'agents/login'
+  end
+
+  post '/login' do
+    #binding.pry
+    @user = Agent.find_by(name: params[:name])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to("/leads")
+    else
+      redirect to("/login")
+    end
+  end
+
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+    end
+    redirect to ("/")
   end
 
 
